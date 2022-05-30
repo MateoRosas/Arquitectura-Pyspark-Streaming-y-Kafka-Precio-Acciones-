d575f4ae099039f8f3079c4407c02c0b2f592074
MateoRosas <mateosrosasm@outlook.es>
2022-05-30 18:34:01 -0500

    Update script files producer and consumer

---
+++ consumer_minmax_stocks.py
@@ -0,0 +1,24 @@
+from kafka import KafkaConsumer
+import json
+
+if __name__ == "__main__":
+   consumer = KafkaConsumer(
+      "registered_user",
+      bootstrap_servers='ip-172-31-12-245.ec2.internal:9092',
+      auto_offset_reset='earliest',
+      group_id="consumer-group-a")
+print("starting the consumer")
+
+#max = 1420000
+#min = 1400000
+
+max = 1407000
+min = 1406100
+for msg in consumer:
+   line = format(json.loads(msg.value)[1])
+   if line != 'PRICE':
+     value = int(line)
+     if value > max:
+            print("Alert!! New Max Value: ", value)
+     elif value < min:
+             print("Alert!! New Min Value: ", value)
